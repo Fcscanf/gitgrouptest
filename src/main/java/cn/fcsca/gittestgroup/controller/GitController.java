642f7ecb6c6e27cdf363e8a29fc1d5755f8360ce
Fcscanf <fcscanf@gmail.com>
2018-09-06 09:09:40 +0800

    创建GitController

---
+++ src/main/java/cn/fcsca/gittestgroup/controller/GitController.java
@@ -0,0 +1,14 @@
+package cn.fcsca.gittestgroup.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * GitController
+ *
+ * @author Fcscanf
+ * @description
+ * @date 上午 9:08 2018-09-06
+ */
+@RestController
+public class GitController {
+}
