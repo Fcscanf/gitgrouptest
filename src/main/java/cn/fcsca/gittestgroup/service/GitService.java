f5d0da9bff2899ac15af3759210e8d71527c00f5
Fcscanf <fcscanf@gmail.com>
2018-09-06 10:22:19 +0800

    功能完善

--- src/main/java/cn/fcsca/gittestgroup/service/GitService.java
+++ src/main/java/cn/fcsca/gittestgroup/service/GitService.java
@@ -1,5 +1,7 @@
 package cn.fcsca.gittestgroup.service;
 
+import cn.fcsca.gittestgroup.dao.GitDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,4 +13,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GitService {
+
+    @Autowired
+    GitDao gitDao;
 }
