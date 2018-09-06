c19c58775e0720b7002492833ded4d7dd9583eaf
Fcscanf <fcscanf@gmail.com>
2018-09-06 08:49:23 +0800

    项目创建

---
+++ src/main/java/cn/fcsca/gittestgroup/GittestgroupApplication.java
@@ -0,0 +1,12 @@
+package cn.fcsca.gittestgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestgroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittestgroupApplication.class, args);
+    }
+}
